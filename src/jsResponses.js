4d086d56b66ab8568554e34506a3da8d8ef0b2a3
Sammi <sdk3150@g.rit.edu>
2023-10-08 21:43:06 -0400

    Post request working, update request under construction

--- src/jsResponses.js
+++ src/jsResponses.js
@@ -1,81 +1,82 @@
-//empty object to store films that users submit
+// empty object to store films that users submit
 const films = {};
 
 const respondJSON = (request, response, status, object, body) => {
-    const headers = {
-        'Content-Type': 'application/json',
-    };
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
-    response.writeHead(status, headers);
+  response.writeHead(status, headers);
 
-    if (!body) {
-        response.write(JSON.stringify(object));
-    }
+  if (body) {
+    response.write(JSON.stringify(object));
+  }
 
-    response.end();
+  response.end();
 };
 
-//head and get request handler
+// head and get request handler
 const getFilms = (request, response, type) => {
-    if (type === 'head') {
-        return respondJSON(request, response, 200, null, false);
-    }
-    const responseJSON = {
-        films,
-    };
+  if (type === 'head') {
+    return respondJSON(request, response, 200, 'null', false);
+  }
+  const responseJSON = {
+    films,
+  };
 
-    return respondJSON(request, response, 200, responseJSON, true);
+  return respondJSON(request, response, 200, responseJSON, true);
 };
 
 const addFilm = (request, response, body) => {
-    const responseJSON = {
-        message: 'Please enter a film name, date, and rating.',
-    };
+  const responseJSON = {
+    message: 'Please enter a film name, date, and rating.',
+  };
 
-    //bad request handler
-    if (!body.name || !body.date || !body.rating) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON, true);
-    }
+  // bad request handler
+  if (!body.name || !body.date || !body.rating) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON, true);
+  }
 
-    let responseCode = 204;
+  let responseCode = 204;
 
-    // If the film isn't in the diary yet
-    if (!films[body.name]) {
-        // Set the status code to 201 (created) and create an empty user
-        responseCode = 201;
-        films[body.name] = {};
-    }
+  // If the film isn't in the diary yet
+  if (!films[body.name]) {
+    // Set the status code to 201 (created) and create an empty user
+    responseCode = 201;
+    films[body.name] = {};
+  }
 
-    // add or update fields for this user name
-    films[body.name].name = body.name;
-    films[body.name].date = body.date;
-    films[body.name].rating = body.rating;
+  // add or update fields for this film
+  films[body.name].name = body.name;
+  films[body.name].date = body.date;
+  films[body.name].rating = body.rating;
+  films[body.name].review = body.review;
+  films[body.name].poster = body.poster;
 
-    // if a film was added for the first time
-    if (responseCode === 201) {
-        responseJSON.message = 'Created Successfully';
-        return respondJSON(request, response, responseCode, responseJSON, true);
-    }
+  // if a film was added for the first time
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON, true);
+  }
 
-    // if not
-    return respondJSON(request, response, responseCode, null, false);
+  // if not
+  console.log(responseCode);
+  console.log(responseJSON);
+  return respondJSON(request, response, responseCode, responseJSON, false);
 };
 
 const notFound = (request, response) => {
-    const responeJSON = {
-        message: 'The page you are looking for was not found.',
-        id: 'notFound',
-    };
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
 
-    respondJSON(request, response, 404, responseJSON, true);
+  respondJSON(request, response, 404, responseJSON, true);
 };
 
 module.exports = {
-    getFilms,
-    addFilm,
-    notFound,
+  getFilms,
+  addFilm,
+  notFound,
 };
-
-
-
--- src/server.js
+++ src/server.js
@@ -4,28 +4,56 @@ const url = require('url');
 const query = require('querystring');
 
 const htmlHandler = require('./htmlResponses.js');
-// const jsonHandler = require('./jsResponses.js');
+const jsonHandler = require('./jsResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// This code taken directly from Parse Body demo in class (auth. Austin Willoughby)
+const parseBody = (request, response, handler) => {
+  // Array to store different components of the request
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addFilm') {
+    parseBody(request, response, jsonHandler.addFilm);
+  }
+};
 const handleGet = (request, response, parsedUrl) => {
   // route to correct method based on url
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
   } else if (parsedUrl.pathname === '/') {
     htmlHandler.getIndex(request, response);
+  } else {
+    jsonHandler.notFound(request, response);
   }
-  // else {
-  //     jsHandler.notFound(request, response);
-  // }
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
   // checks handlers
-  // if (request.method === 'POST') {
-  //     handlePost(request, response, parsedUrl);
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  }
   if (request.method === 'GET') {
     handleGet(request, response, parsedUrl);
   }
